76090555b4593eb4b91745ebaf117e1c66c87822
mayara <mayarasalla4@gamil.com>
2023-12-12 11:49:00 -0300

    inicial

---
+++ Desktop/Somativa/Somativa/Migrations/20231206144500_teste.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Somativa.Migrations
+{
+    /// <inheritdoc />
+    public partial class teste : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Imagem",
+                table: "tbProdutos",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Imagem",
+                table: "tbProdutos",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
